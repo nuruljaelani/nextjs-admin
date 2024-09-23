784ae9d2e0409ac46f805deb55402bd69372ab9d
Nurul Jaelani <nuruljaelani96@gmail.com>
2024-09-23 11:10:27 +0700

    feat: next auth

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|login|register|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
